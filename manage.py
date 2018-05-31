1c71bc0c9fa30b6894eae9da1e31882b36dca2a4
xxqyjk <xxqyjk@163.com>
2018-05-31 19:00:27 +0800

    立项

--- manage.py
+++ manage.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_xinwen'
+
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -8,5 +19,5 @@ def index():
     return 'index'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
